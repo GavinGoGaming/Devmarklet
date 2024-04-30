3df790dd5f329f6dd580b82125f78393a91f029d
Gavin Fox <gavingogaming@gmail.com>
2024-04-30 09:18:49 -0700

    feat: Font arial

--- pxtvp/source.js
+++ pxtvp/source.js
@@ -9,7 +9,7 @@ const svg = `<style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</
 
 // Add UI
 document.body.insertAdjacentHTML('afterbegin', `
-<div style="background:#222;color:white;position:absolute;top:10px;left:10px;z-index:1000;padding:10px;border:1px solid #ffffff50;border-radius:10px;margin:0;width:250px;" id="pxtvp">
+<div style="font-family:Arial;background:#222;color:white;position:absolute;top:10px;left:10px;z-index:1000;padding:10px;border:1px solid #ffffff50;border-radius:10px;margin:0;width:250px;" id="pxtvp">
 <b>px to vp</b><br/>by <a href="https://github.com/gavingogaming" target="_blank">gavin</a></br><input id="PVpx" type="number" placeholder="Pixel Amount" style="background:#222;color:white;border:1px solid #ffffff20;width:100%;border-radius:2px;"><br/><button style="background:#111;color:white;border:1px solid #ffffff20;width:100%;border-radius:5px;" id="PVwidth">Calc. Width</button><br/><button style="background:#111;color:white;border:1px solid #ffffff20;width:100%;border-radius:5px;" id="PVheight">Calc. Height</button>
 <span id="PVrew">Width: 0vw</span> <span id="PVcopyw"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve" width="16">${svg}</svg></span><br/>
 <span id="PVre">Height: 0vh</span> <span id="PVcopyh"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve" width="16">${svg}</svg></span>
